dfd7394c58ca3e6b3514dead37f35b528259f729
matrhe13 <matrhe13@gmail.com>
2025-05-24 11:29:17 +0200

    Create 2X: Zusätzliche Übungen.py

---
+++ 2X: Zusätzliche Übungen.py
@@ -0,0 +1,25 @@
+#Programmierübung: Bevölkerungswachstum
+populationIn2012 = 1000
+populationIn2013 = populationIn2012 * 1.1
+populationIn2014 = populationIn2013 * 1.1
+populationIn2015 = populationIn2014 * 1.1
+
+#Multiple-Choice-Übung: Vereinfachung
+x + 5
+
+#Programmierübung: Komplikation
+print(A+B-max(A,B))
+
+#Programmierübung: Auszahlungsrechner
+print(min(max(balance*0.021, 10),balance))
+
+#Sortierübung: Sortier-Durcheinander
+1: tmp = max(x, y)
+2: x = min(x, y)
+3: y = tmp
+4: tmp = max(y, z)
+5: y = min(y, z)
+6: z = tmp
+7: tmp = max(x, y)
+8: x = min(x, y)
+9:y = tmp
