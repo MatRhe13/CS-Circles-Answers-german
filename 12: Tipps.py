2ba95048469dff44203e94ab5465de71c65c0e3a
matrhe13 <matrhe13@gmail.com>
2025-05-24 12:17:37 +0200

    Create 12: Tipps.py

---
+++ 12: Tipps.py
@@ -0,0 +1,13 @@
+#Programmierübung: Alphabet Aerobics
+for c in range(0, 26):
+ print(chr(ord('A')+c), end='')
+
+#Programmierübung: Lucky Sevens
+for i in range(17,100,10):
+   print(i)
+
+#Sortierübung: One None
+1: «blank line»
+2: None
+3: <class 'type'>
+4: <class 'NoneType'>
