afdef96da023f842427bba6b58e19567737da00b
matrhe13 <matrhe13@gmail.com>
2025-05-24 11:32:47 +0200

    Create 3: Kommentare und Anführungszeichen.py

---
+++ 3: Kommentare und Anführungszeichen.py
@@ -0,0 +1,12 @@
+#Programmierübung: Second Guessing
+SekundenProMinute = 60
+SekundenProStunde = SekundenProMinute * 60
+SekundenProTag = SekundenProStunde * 24
+TageProWoche = 7
+print(SekundenProTag * TageProWoche)
+
+#Multiple-Choice-Übung: Maskierungszeichen
+Backslashes \ and single quotes ' and double quotes " and pound signs # are awesome!
+
+#Programmierübung: The Great Escape
+print('A double-quote\'s escaped using a backslash, e.g. \\"')
