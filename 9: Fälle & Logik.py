0c31c3fe1a48cea3cce31a008057462ca1b586da
matrhe13 <matrhe13@gmail.com>
2025-05-24 12:08:23 +0200

    Create 9: Fälle & Logik.py

---
+++ 9: Fälle & Logik.py
@@ -0,0 +1,30 @@
+#Programmierübung: Absoluter Wert
+x=int(input())
+if x >= 0:
+   print(x)
+else:
+   print(-x)
+
+#Programmierübung: First, Second, Third
+x = int(input())
+if x==1:
+   print("1st")
+elif x == 2:
+   print("2nd")
+elif x == 3:
+   print("3rd")
+else:
+   print(str(x)+"th")
+
+#Programmierübung: 26 Letters
+letter = input()
+if letter>=chr(65) and letter<=chr(90):
+    print((ord(letter)-64))
+else:
+    print('invalid')
+
+#Multiple-Choice-Übung: De Morgansche Gesetze
+not ((not A) and (not B))
+
+#Multiple-Choice-Übung: Operatorrangfolge
+False
