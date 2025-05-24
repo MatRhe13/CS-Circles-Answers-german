b82ab03e256ea3b7e3d6d49e20219d5f16b4d73e
matrhe13 <matrhe13@gmail.com>
2025-05-24 11:14:35 +0200

    Create 2: Funktionen.py

---
+++ 2: Funktionen.py
@@ -0,0 +1,18 @@
+#Multiple-Choice-Übung: Min and Max I
+26 7
+
+#Kurzübung: Min and Max II
+9
+
+#Programmierübung: Eine Straße
+print(min(a,b,c))
+
+#Programmierübung: Zwei Straßen
+q=min(a,b,c)
+s=min(d,e)
+print(max(q,s))
+
+#Sortierübung: Sortieren nach Größe
+1: print(min(x, y, z))
+2: print(x+y+z-min(x, y, z)-max(x, y, z))
+3: print(max(x, y, z))
