b6aab8689d32ab26916c425cf8403f878128ab4e
matrhe13 <matrhe13@gmail.com>
2025-05-24 12:10:09 +0200

    Create 10: def.py

---
+++ 10: def.py
@@ -0,0 +1,10 @@
+#Programmierübung: Cubism
+def cube(n):
+   return n*n*n
+
+#Multiple-Choice-Übung: In-n-Out
+2 lines: 'not in f', then 'in f'
+
+#Programmierübung: Rectangle
+def rectanglePerimeter(width, height):
+   return width+height+width+height
