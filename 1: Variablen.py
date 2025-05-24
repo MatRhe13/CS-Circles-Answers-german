dda47c0d8fae6c12614fee3f696fad898c648336
matrhe13 <matrhe13@gmail.com>
2025-05-24 11:08:37 +0200

    Update 1: Variablen.py

--- 1: Variablen.py
+++ 1: Variablen.py
@@ -1,26 +1,19 @@
-Kurzübung: x Marks the Spot:
-Antwort: 15
+#Kurzübung: x Marks the Spot:
+15
 
-###
-
-Programmierübung: Python-Anatomie:
+#Programmierübung: Python-Anatomie:
 heads = people
 shoulders = people*2
 knees = shoulders
 toes = people*10
 
-###
-
-Sortierübung: Geschwindigkeitsberechnung:
+#Sortierübung: Geschwindigkeitsberechnung:
 1: totalTime = uphillTime + downhillTime
 2:totalDistance = uphillDistance + downhillDistance
 3: averageSpeed = totalDistance / totalTime
 4: print(averageSpeed)
 
-###
-
-Programmierübung: Variablen vertauschen:
-
+#Programmierübung: Variablen vertauschen:
 xO = x
 yO = y
 x=y
